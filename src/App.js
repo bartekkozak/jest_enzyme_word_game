b34678330bb65687faf9d3e729374a7fca8c11ee
bartek <bartekkozak3@gmail.com>
2019-05-18 01:08:03 +0200

    Add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import GuessedWords from "./components/GuessedWord/GuessedWords";
+import Congrats from "./components/Congrats/Congrats";
 
 function App() {
   return (
-    <div className="App">
-      <p>TEST</p>
+    <div className="container">
+      <h1>Word Game</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+      />
     </div>
   );
 }
