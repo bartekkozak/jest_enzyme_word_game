8ede8f30db1d11346c9d6dd92186a0632c0d2abb
bartek <bartekkozak3@gmail.com>
2019-05-19 16:46:50 +0200

    Write code and tests for successReducer

--- src/store/reducers/successReducer.js
+++ src/store/reducers/successReducer.js
@@ -1,3 +1,12 @@
-export default (state, action) => {
-  return null;
+import { CORRECT_GUESS } from "../actions/actionTypes";
+
+const initialState = false;
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
